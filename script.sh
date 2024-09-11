b93c37f5ff7588fedaed11b687beec3cff1fb5b8
Ricardo Fahham <rfahham@gmail.com>
2024-09-11 19:28:59 -0300

    script para instalação

--- script.sh
+++ script.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+sudo apt install python3.10-venv
 python3 -m venv ./venv && source venv/bin/activate
 
-# pip install robotframework
-# pip install webdriver-manager 
-# pip install robotframework-seleniumlibrary
+sudo pip install robotframework
+sudo pip install webdriver-manager 
+sudo pip install robotframework-seleniumlibrary
 
-# pip list
+echo "Versão do Python"
+python3 --version
 
-# python3 --version
-# pip --version
-# robot --version
+echo "Versão do PIP"
+pip --version
+
+echo "Versão do Robot"
+robot --version
